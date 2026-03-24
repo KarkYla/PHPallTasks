9a4c390c0e3ba8d480c790312a66e0f992fdbd0a
KarkYla <he_he_08@mail.ru>
2026-03-25 00:35:26 +0300

    lw3-1 zadanie-1 variant-2

---
+++ lw3-1.php
@@ -0,0 +1,23 @@
+<?php
+
+function groupUsersByRole(array $users): array
+{
+    $result = [];
+
+    foreach ($users as $user) {
+        $role = $user['role'];
+        $name = $user['name'];
+
+        $result[$role][] = $name;
+    }
+    return $result;
+}
+
+$users = [
+    ['name' => 'Ivan', 'role' => 'admin'],
+    ['name' => 'Petr', 'role' => 'user'],
+    ['name' => 'Anna', 'role' => 'admin'],
+];
+
+$groupes = groupUsersByRole($users);
+print_r($groupes);
